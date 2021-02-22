c72358beef30adaa1294a42945ed022fdbd534fa
Georgi Yolovski <georgi.yolovski@accedia.com>
2021-02-22 17:14:52 +0200

    Mapped the simpler tables

--- tap_grader/service.py
+++ tap_grader/service.py
@@ -17,22 +17,47 @@ class GraderReportingService:
         authSource = config['mongoAuthSource']
         conn_string = f'mongodb://{user}:{password}@{host}:{port}/?authSource={authSource}'
         self.client = MongoClient(conn_string)
+        self.schema_map = {
+            'advertiser_data': 'AdvertiserData',
+            'batch_job': 'BatchJob',
+            'dashboard': 'Dashboard',
+            'google_my_business_data': 'GoogleMyBusinessData',
+            'manage_keywords_action': 'ManageKeywordsAction',
+            'marketing_attributes_data': 'MarketingAttributesData',
+            'potential_lead': 'PotentialLead',
+            'targeted_email_price_sheet': 'TargetedEmailPriceSheet',
+            'user_profile': 'UserProfile'
+        }
 
-    
     def get_reports(self):
-        for doc in self.client.proposal_tool['AdvertiserData'].find():
+        for doc in self.client.proposal_tool[self.schema_map[self.stream]].find():
             singer.write_record(self.stream, self.map_record(doc))
     def map_record(self, doc):
         record = {}
         for prop in self.props.items():
             prop_type = prop[1]['type']
             prop_name = prop[0]
-            val = ''
-            if prop_type == 'integer':
-                val = int(doc[prop_name]) if doc.get(prop_name) else ''
-            elif prop_type == 'number':
-                val = float(doc[prop_name]) if doc.get(prop_name) else ''
-            else:
-                val = str(doc.get(prop_name, ''))
-            record[prop_name] = val
+            record[prop_name] = self.get_property(prop_name, prop_type, doc)
         return record
+
+    def get_property(self, prop_name, prop_type, doc):
+        nested = '_' in prop_name and prop_name != '_id'
+        val = ''
+        if nested:
+            path_items = list(filter(lambda x: x != '', prop_name.split('_')))
+            val = doc
+            for item in path_items:
+                val = val.get(item if item != 'id' else '_id', {})
+            val = val if val else ''
+        else:
+            val = doc.get(prop_name, '')
+
+        if prop_type == 'integer':
+            val = int(val) if val else ''
+        elif prop_type == 'number':
+            val = float(val) if val else ''
+        else:
+            if isinstance(val, list):
+                val = ','.join(val)
+            val = str(val).replace('\n', '\\n').replace('\r', '\\r').replace('\0', '').replace('\x00', '')
+        return val
