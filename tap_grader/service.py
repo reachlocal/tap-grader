afbb7c121fa4f33377b336c6a3ca5acb78a3c556
Georgi Yolovski <georgi.yolovski@accedia.com>
2021-02-23 15:55:25 +0200

    Added mapping for SIlktide and initial mapping for proposals

--- tap_grader/service.py
+++ tap_grader/service.py
@@ -26,18 +26,36 @@ class GraderReportingService:
             'marketing_attributes_data': 'MarketingAttributesData',
             'potential_lead': 'PotentialLead',
             'targeted_email_price_sheet': 'TargetedEmailPriceSheet',
-            'user_profile': 'UserProfile'
+            'user_profile': 'UserProfile',
+            'silktide_data': 'SilktideData',
+            'proposal': 'Proposal'
         }
 
     def get_reports(self):
         for doc in self.client.proposal_tool[self.schema_map[self.stream]].find():
-            singer.write_record(self.stream, self.map_record(doc))
+            record = self.map_proposal_record(doc) if self.stream == 'proposal' else self.map_record(doc)
+            singer.write_record(self.stream, record)
+
     def map_record(self, doc):
         record = {}
         for prop in self.props.items():
             prop_type = prop[1]['type']
             prop_name = prop[0]
             record[prop_name] = self.get_property(prop_name, prop_type, doc)
+        return record
+
+    def map_proposal_record(self, doc):
+        scores = doc.get('scores', [])
+        if len(scores) > 0:
+            doc['scores'] = list(filter(lambda x: not x['advertiser']['isCompetitor'], scores))[0]
+        else:
+            doc['scores'] = {}
+        if doc.get('solutions') and len(doc.get('solutions', [])) > 0:
+            doc['solutions'] = list(map(lambda x: x['name'], doc['solutions']))
+        record = self.map_record(doc)
+
+        # custom mapping logic here
+
         return record
 
     def get_property(self, prop_name, prop_type, doc):
@@ -47,7 +65,7 @@ class GraderReportingService:
             path_items = list(filter(lambda x: x != '', prop_name.split('_')))
             val = doc
             for item in path_items:
-                val = val.get(item if item != 'id' else '_id', {})
+                val = val.get(item if item != 'id' else '_id', {}) if val else val
             val = val if val else ''
         else:
             val = doc.get(prop_name, '')
